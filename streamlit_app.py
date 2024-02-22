73fb60a9b3742346ccd08b23f1556f94856efc02
ankitgupta14011998 <32798626+ankitgupta14011998@users.noreply.github.com>
2024-02-23 02:04:32 +0530

    added input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Takes json data and normalizes it to key value structure
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
